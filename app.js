15c8c3428cbd6be7b28c279b689fdae1df088f3e
hryt430 <yuta430hr@icloud.com>
2024-10-16 22:26:58 +0900

    Revert "Revert "update tetris-file""

--- app.js
+++ app.js
@@ -1,1 +1,78 @@
+const canvas = document.querySelector("#tetris");
+const context = canvas.getContext("2d");
+context.scale(20, 20);
 
+const draw = () => {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+const createPiece = (type) => {
+    if (type === "T") {
+      return [
+        [1, 1, 1],
+        [0, 1, 0],
+      ];
+    } else if (type === "O") {
+      return [
+        [2, 2],
+        [2, 2],
+      ];
+    } else if (type === 'L') {
+      return [
+        [0, 3, 0],
+        [0, 3, 0],
+        [0, 3, 3],
+      ];
+    } else if (type === 'J') {
+      return [
+        [0, 4, 0],
+        [0, 4, 0],
+        [4, 4, 0],
+      ];
+    } else if (type === 'I') {
+      return [
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+      ];
+    } else if (type === 'S') {
+      return [
+        [0, 6, 6],
+        [6, 6, 0],
+        [0, 0, 0],
+      ];
+    } else if (type === 'Z') {
+      return [
+        [7, 7, 0],
+        [0, 7, 7],
+        [0, 0, 0],
+      ];
+    }
+  }
+
+  const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF',
+  ];
+
+
+const drawMatrix = (matrix, offset) => {
+    matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          context.fillStyle = colors[value];
+          context.fillRect(x + offset.x, y + offset.y, 1, 1);
+        }
+      });
+    });
+  }
+
+  drawMatrix(createPiece('T'), { x: 5, y: 5 });
